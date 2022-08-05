c0f445a9cbda351aea73d5ecf1902a674106ba00
HARSH RAJENDRA UPADHYE <89975347+Harshu467@users.noreply.github.com>
2022-08-05 21:21:24 +0530

    App js updated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Hero from './Components/Hero/Hero';
+import Program from './Components/Programs/Program';
 function App() {
   return (
     <div className="App">
           <span style={{color: 'white'}}>
             <Hero/>
+            <Program/>
           </span>
     </div>
   );
