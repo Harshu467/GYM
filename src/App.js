32c5d75fa56a032017b44c657faf0a0df6e22c10
HARSH RAJENDRA UPADHYE <89975347+Harshu467@users.noreply.github.com>
2022-08-05 17:00:01 +0530

    Updated

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Hero from './Components/Hero/Hero';
+function App() {
+  return (
+    <div className="App">
+          <span style={{color: 'white'}}>
+            <Hero/>
+          </span>
+    </div>
+  );
+}
+
+export default App;
