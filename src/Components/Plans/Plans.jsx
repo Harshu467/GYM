0f5e5ed0da7860b4f222a4350d288243ce23ec5a
HARSH RAJENDRA UPADHYE <89975347+Harshu467@users.noreply.github.com>
2022-08-07 17:56:09 +0530

    Updated

--- src/Components/Plans/Plans.jsx
+++ src/Components/Plans/Plans.jsx
@@ -4,13 +4,13 @@ import {plansData} from '../../data/plansData.js'
 import whitetick from '../../assets/whiteTick.png'
 const Plans = () => {
   return (
-    <div className='plans-container'>
+    <div className='plans-container' >
         <div className="programs-header" style={{gap:'2rem'}} >
             <span className='stroke-text'>READY TO START</span>
             <span>YOUR JOURNEY</span>
             <span className='stroke-text'>NOW WITH US</span>
         </div>
-        <div className="plans">
+        <div className="plans" id='Plans'>
             {plansData.map((plan,i)=>(
                 <div className="plan" key={i}>
                     {plan.icon}
