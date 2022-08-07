03e1f03240a34c11ff4ac761537e5cc07c5010cd
HARSH RAJENDRA UPADHYE <89975347+Harshu467@users.noreply.github.com>
2022-08-07 14:40:02 +0530

    Footer jsx updated

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,11 +1,25 @@
 import React from 'react'
-
+import github from '../../assets/github.png'
+import instagram from '../../assets/instagram.png'
+import linkedin from '../../assets/linkedin.png'
+import logo from '../../assets/logo.png'
+import './Footer.css'
 const Footer = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+    return (
+        <div className='footer-container'>
+            <hr />
+            <div className="footer">
+                <div className="social-links">
+                    <img src={github} alt="" />
+                    <img src={instagram} alt="" />
+                    <img src={linkedin} alt="" />
+                </div>
+                <div className="logo-f">
+                    <img src={logo} alt="" />
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Footer
