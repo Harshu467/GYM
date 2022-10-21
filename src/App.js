093877a4c1c52e45fb9d9282c6b58e3e158df3e9
Harsh Upadhye <harshupadhye2832@gmail.com>
2022-10-21 10:27:55 +0530

    Updated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import './App.css';
-import Footer from './Components/Footer/Footer';
-import Hero from './Components/Hero/Hero';
-import Join from './Components/Join/Join';
-import Plans from './Components/Plans/Plans';
-import Program from './Components/Programs/Program';
-import Reason from './Components/Reasons/Reason';
-import Testimonials from './Components/Testimonials/Testimonials';
-function App() {
+import React from 'react';
+import Sign from './Components/Sign/Sign';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import Home from './Components/Home/Home';
+
+const App=()=> {
   return (
-    <div className="App">
-          <span style={{color: 'white'}}>
-            <Hero/>
-            <Program/>
-            <Reason/>
-            <Plans/>
-            <Testimonials/>
-            <Join/>
-            <Footer/>
-          </span>
+    <div className="App"> 
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Sign />} />
+          <Route path='/Home' element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
