b4d0163fb39197b21ecd8036a513726e6e961e2f
Harsh Upadhye <harshupadhye2832@gmail.com>
2022-10-21 10:28:24 +0530

    Updated Sign

--- src/Components/Sign/Sign.jsx
+++ src/Components/Sign/Sign.jsx
@@ -4,7 +4,7 @@ import './Sign.css'
 const Sign = () => {
     const [signIn, toggle] = useState(true);
     return (
-        <>
+        <div>
             <div className="Container">
                 <div className="SignUpContainer" signIn={signIn}>
                     <form className="Form">
@@ -21,37 +21,34 @@ const Sign = () => {
                         <input type={'text'} placeholder='Name' />
                         <input type={'email'} placeholder='Email' />
                         <input type={'password'} placeholder='Password' />
-                        <a href='#'>Forgot your password?</a>
+                        <a className='Anchor' href='#'>Forgot your password?</a>
                         <button>Sign In</button>
                     </form>
                 </div>
                 <div className="OverlayContainer" signinIn={signIn}>
                     <div className="Overlay" signinIn={signIn}>
-
-                        <div className="LeftOverlayPanel" signinIn={signIn}>
+                        <div className="OverlayPanel LeftOverlayPanel" signinIn={signIn}>
                             <h1 className="Title">Welcome Back!</h1>
                             <p className="Paragraph">
                                 To keep connected with us please login with your personal info
                             </p>
-                            <button className="GhostButton" onClick={() => toggle(true)}>
+                            <button className="Button GhostButton" onClick={() => toggle(true)}>
                                 Sign In
                             </button>
                         </div>
-
-                        <div className="RightOverlayPanel" signinIn={signIn}>
+                        <div className="OverlayPanel RightOverlayPanel" signinIn={signIn}>
                             <h1 className="Title">Hello, Friend!</h1>
                             <p className="Paragraph">
                                 Enter Your personal details and start journey with us
                             </p>
-                            <button className="GhostButton" onClick={() => toggle(false)}>
+                            <button className="Button GhostButton" onClick={() => toggle(false)}>
                                 Sigin Up
                             </button>
                         </div>
-
                     </div>
                 </div>
             </div>
-        </>
-            )
-}
+        </div>
+);
+};
 export default Sign
